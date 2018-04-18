200903837d126cca37309537eaf8f9429a8d6e83
Yachen <yachenlin1203@gmail.com>
2018-04-18 14:38:06 -0400

    Almost finish

--- week4/src/Board.java
+++ week4/src/Board.java
@@ -1,16 +1,33 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Board class.
+ *
  * @author Yachen Lin
  */
 public final class Board {
     private final int dimension;
-    private int[][] blocks;
-    private int numMoves;
+    int numMoves;
+    private final int[][] blocks;
+
+
+    public Board getPredecessor() {
+        return predecessor;
+    }
+
+    public void setPredecessor(Board predecessor) {
+        this.predecessor = predecessor;
+    }
+
+    private Board predecessor;
+    private int mDist;
 
     /**
      * Construct a board from an n-by-n array of blocks.
@@ -20,7 +37,44 @@ public final class Board {
     public Board(int[][] blocks) {
         dimension = blocks.length;
         this.blocks = blocks;
+
+        mDist = manhattan();
         numMoves = 0;
+    }
+
+    /**
+     * Unit tests (not graded).
+     *
+     * @param args command line input
+     */
+    public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+
+    }
+
+
+
+    public int getmDist() {
+        return mDist;
     }
 
     /**
@@ -102,7 +156,6 @@ public final class Board {
      */
     public Board twin() {
         return null;
-
     }
 
     /**
@@ -111,9 +164,69 @@ public final class Board {
      * @param y board y
      * @return true or false
      */
+    @Override
     public boolean equals(Object y) {
-        return false;
+        if (!(y instanceof Board)) {
+            return false;
+        }
+        Board b = (Board) y;
+        if (b.dimension() != dimension) {
+            return false;
+        }
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if (blocks[i][j] != b.blocks[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
+    /**
+     * Returns a hash code value for the object. This method is
+     * supported for the benefit of hash tables such as those provided by
+     * {@link HashMap}.
+     * <p>
+     * The general contract of {@code hashCode} is:
+     * <ul>
+     * <li>Whenever it is invoked on the same object more than once during
+     * an execution of a Java application, the {@code hashCode} method
+     * must consistently return the same integer, provided no information
+     * used in {@code equals} comparisons on the object is modified.
+     * This integer need not remain consistent from one execution of an
+     * application to another execution of the same application.
+     * <li>If two objects are equal according to the {@code equals(Object)}
+     * method, then calling the {@code hashCode} method on each of
+     * the two objects must produce the same integer result.
+     * <li>It is <em>not</em> required that if two objects are unequal
+     * according to the {@link Object#equals(Object)}
+     * method, then calling the {@code hashCode} method on each of the
+     * two objects must produce distinct integer results.  However, the
+     * programmer should be aware that producing distinct integer results
+     * for unequal objects may improve the performance of hash tables.
+     * </ul>
+     * <p>
+     * As much as is reasonably practical, the hashCode method defined by
+     * class {@code Object} does return distinct integers for distinct
+     * objects. (This is typically implemented by converting the internal
+     * address of the object into an integer, but this implementation
+     * technique is not required by the
+     * Java&trade; programming language.)
+     *
+     * @return a hash code value for this object.
+     * @see Object#equals(Object)
+     * @see System#identityHashCode
+     */
+    @Override
+    public int hashCode() {
+        int result = 0;
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                result += 31 * blocks[i][j];
+            }
+        }
+        return result % (dimension * dimension);
     }
 
     /**
@@ -122,8 +235,13 @@ public final class Board {
      * @return all neighboring boards
      */
     public Iterable<Board> neighbors() {
-        return null;
-
+        Board currentBoard = this;
+        return new Iterable<Board>() {
+            @Override
+            public Iterator<Board> iterator() {
+                return new BoardIterator(currentBoard);
+            }
+        };
     }
 
     /**
@@ -132,36 +250,103 @@ public final class Board {
      * @return string representation of this board (in the output format specified below)
      */
     public String toString() {
-        return null;
-
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("\n%d\n", dimension));
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                sb.append(String.format(" %d ", blocks[i][j]));
+            }
+            sb.append(String.format("\n"));
+        }
+        sb.append("\n");
+        return sb.toString();
     }
 
-    /**
-     * Unit tests (not graded).
-     *
-     * @param args command line input
-     */
-    public static void main(String[] args) {
-        // create initial board from file
-        In in = new In(args[0]);
-        int n = in.readInt();
-        int[][] blocks = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                blocks[i][j] = in.readInt();
-        Board initial = new Board(blocks);
+    private static class BoardIterator implements Iterator<Board> {
+        private Board originalBoard;
+        private List<int[]> neighbors;
+        private int idx;
+        private int[] hole;
 
-        // solve the puzzle
-        Solver solver = new Solver(initial);
+        /**
+         * Constructor.
+         *
+         * @param board
+         */
+        private BoardIterator(Board board) {
+            originalBoard = board;
+            neighbors = new ArrayList<>();
+            hole = findHole();
+            if (hole == null) {
+                throw new IllegalArgumentException();
+            }
+            int[][] neis = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+            int dimension = originalBoard.dimension();
+            for (int[] neighbor : neis) {
+                neighbor[0] += hole[0];
+                neighbor[1] += hole[1];
+                if (neighbor[0] < 0 || neighbor[0] >= dimension
+                        || neighbor[1] < 0 || neighbor[1] >= dimension) {
+                    continue;
+                }
+                neighbors.add(new int[]{neighbor[0], neighbor[1]});
+            }
 
-        // print solution to standard output
-        if (!solver.isSolvable())
-            StdOut.println("No solution possible");
-        else {
-            StdOut.println("Minimum number of moves = " + solver.moves());
-            for (Board board : solver.solution())
-                StdOut.println(board);
+            idx = 0;
+
         }
 
+        private static void swap(Board originalBoard, int[] x, int[] y) {
+            int tmp = originalBoard.blocks[x[0]][x[1]];
+            originalBoard.blocks[x[0]][x[1]] = originalBoard.blocks[y[0]][y[1]];
+            originalBoard.blocks[y[0]][y[1]] = tmp;
+        }
+
+        private int[] findHole() {
+            for (int i = 0; i < originalBoard.dimension(); i++) {
+                for (int j = 0; j < originalBoard.dimension(); j++) {
+                    if (originalBoard.blocks[i][j] == 0) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+            return null;
+        }
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return idx < neighbors.size();
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public Board next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+
+            int[][] blocks = new int[originalBoard.dimension()][originalBoard.dimension()];
+            for (int i = 0; i < blocks.length; i++) {
+                System.arraycopy(originalBoard.blocks[i], 0, blocks[i], 0, blocks[0].length);
+            }
+            Board result = new Board(blocks);
+            result.numMoves = originalBoard.numMoves + 1;
+            swap(result, neighbors.get(idx), hole);
+            idx++;
+            return result;
+        }
     }
 }
--- week4/src/Solver.java
+++ week4/src/Solver.java
@@ -1,45 +1,28 @@
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Solver class.
+ *
  * @author Yachen
  */
 public class Solver {
+    private MinPQ<Tuple> minPQ;
+    private int moves;
+
     /**
      * Find a solution to the initial board (using the A* algorithm).
      *
      * @param initial Find a solution to the initial board (using the A* algorithm)
      */
     public Solver(Board initial) {
-
-    }
-
-    /**
-     * Is the initial board solvable?
-     *
-     * @return Is the initial board solvable?
-     */
-    public boolean isSolvable() {
-        return false;
-
-    }
-
-    /**
-     * Min number of moves to solve initial board; -1 if unsolvable.
-     *
-     * @return Min number of moves to solve initial board; -1 if unsolvable.
-     */
-    public int moves() {
-        return 0;
-
-    }
-
-    /**
-     * Sequence of boards in a shortest solution; null if unsolvable.
-     *
-     * @return Sequence of boards in a shortest solution; null if unsolvable
-     */
-    public Iterable<Board> solution() {
-        return null;
-
+        minPQ = new MinPQ<>(new MyComparator());
+        initial.setPredecessor(null);
+        minPQ.insert(new Tuple(initial, new ArrayList<>()));
+        solution = solve();
     }
 
     /**
@@ -49,5 +32,113 @@ public class Solver {
      */
     public static void main(String[] args) {
 
+    }
+
+    /**
+     * Is the initial board solvable?
+     *
+     * @return Is the initial board solvable?
+     */
+    public boolean isSolvable() {
+        return true;
+
+    }
+
+    /**
+     * Min number of moves to solve initial board; -1 if unsolvable.
+     *
+     * @return Min number of moves to solve initial board; -1 if unsolvable.
+     */
+    public int moves() {
+        return moves;
+    }
+
+    private Iterable<Board> solution;
+    /**
+     * Sequence of boards in a shortest solution; null if unsolvable.
+     *
+     * @return Sequence of boards in a shortest solution; null if unsolvable
+     */
+    public Iterable<Board> solution() {
+        return solution;
+    }
+
+    private Iterable<Board> solve() {
+        while (!minPQ.isEmpty()) {
+            Tuple t = minPQ.delMin();
+            Board current = t.board;
+            List<Board> path = t.path;
+            Board prev = current.getPredecessor();
+            path.add(current);
+            if (current.isGoal()) {
+                moves = current.numMoves;
+                return t.path;
+            }
+            for (Board neighbor : current.neighbors()) {
+                if (!(neighbor.equals(prev))) {
+                    neighbor.setPredecessor(current);
+                    minPQ.insert(new Tuple(neighbor, path));
+                }
+            }
+        }
+        return null;
+    }
+
+    private static class Tuple {
+        Board board;
+
+        List<Board> path;
+
+        public Tuple(Board board, List<Board> path) {
+            this.board = board;
+            this.path = path;
+        }
+    }
+
+    private class MyComparator implements Comparator<Tuple> {
+        /**
+         * Compares its two arguments for order.  Returns a negative integer,
+         * zero, or a positive integer as the first argument is less than, equal
+         * to, or greater than the second.<p>
+         * <p>
+         * In the foregoing description, the notation
+         * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+         * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+         * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+         * <i>expression</i> is negative, zero or positive.<p>
+         * <p>
+         * The implementor must ensure that <tt>sgn(compare(x, y)) ==
+         * -sgn(compare(y, x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
+         * implies that <tt>compare(x, y)</tt> must throw an exception if and only
+         * if <tt>compare(y, x)</tt> throws an exception.)<p>
+         * <p>
+         * The implementor must also ensure that the relation is transitive:
+         * <tt>((compare(x, y)&gt;0) &amp;&amp; (compare(y, z)&gt;0))</tt> implies
+         * <tt>compare(x, z)&gt;0</tt>.<p>
+         * <p>
+         * Finally, the implementor must ensure that <tt>compare(x, y)==0</tt>
+         * implies that <tt>sgn(compare(x, z))==sgn(compare(y, z))</tt> for all
+         * <tt>z</tt>.<p>
+         * <p>
+         * It is generally the case, but <i>not</i> strictly required that
+         * <tt>(compare(x, y)==0) == (x.equals(y))</tt>.  Generally speaking,
+         * any comparator that violates this condition should clearly indicate
+         * this fact.  The recommended language is "Note: this comparator
+         * imposes orderings that are inconsistent with equals."
+         *
+         * @param o1 the first object to be compared.
+         * @param o2 the second object to be compared.
+         * @return a negative integer, zero, or a positive integer as the
+         * first argument is less than, equal to, or greater than the
+         * second.
+         * @throws NullPointerException if an argument is null and this
+         *                              comparator does not permit null arguments
+         * @throws ClassCastException   if the arguments' types prevent them from
+         *                              being compared by this comparator.
+         */
+        @Override
+        public int compare(Tuple o1, Tuple o2) {
+            return Integer.compare(o1.board.getmDist(), o2.board.getmDist());
+        }
     }
 }
